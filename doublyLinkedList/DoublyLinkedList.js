ce25eaa885ad62d5af1d19dd9f54a4d302a8d420
Luis Aleman <Luis@toppatch.com>
2014-01-09 13:24:39 -0500

    Doubly Linked Lists

---
+++ doublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,195 @@
+/*
+ * Author: Luis Aleman
+ */
+
+/**
+ * @method node
+ * @constructor
+ * @param content {*}
+ * @param [next] {Node|undefined} Next node reference, or null
+ * @param [prev] {Node|undefined} Previous node reference, or null
+ * @returns {Node}
+ */
+function Node (content, next, prev) {
+    'use strict';
+    this.content = content;
+    this.next = next;
+    this.prev = prev;
+    return this;
+}
+
+/**
+ * @method DoublyLinkedList
+ * @returns {DoublyLinkedList}
+ * @constructor
+ */
+function DoublyLinkedList() {
+    'use strict';
+    /**
+     * Count of nodes in list
+     * @property _length
+     * @type {number}
+     * @private
+     */
+    this._length = 0;
+
+    /**
+     * Reference to the head node
+     * @property _head
+     * @type {null}
+     * @private
+     */
+    this._head = undefined;
+
+    /**
+     * reference to the tail node
+     * @property _tail
+     * @type {null}
+     * @private
+     */
+    this._tail = undefined;
+
+    return this;
+}
+
+DoublyLinkedList.prototype = {
+    /**
+     * Add a new Node at the end of the DoublyLinkedList.
+     * Alias
+     * @method push
+     * @param content
+     * @returns {DoublyLinkedList}
+     * @chainable
+     */
+    push: function (content) {
+        'use strict';
+        // Create new node, where next is undefined, and previous is _tail
+        var node = new Node(content, undefined, this._tail);
+
+        if (this._length === 0) {
+            this._head = this._tail = node;
+        } else {
+            this._tail.next = node;
+            this._tail = node;
+        }
+
+        // No matter the content, this method will always add a Node
+        this._length += 1;
+
+        return this;
+    },
+
+    /**
+     * Add a new Node at the beginning of the DoublyLinkedList.
+     * @method unshift
+     * @param content
+     * @returns {DoublyLinkedList}
+     * @chainable
+     */
+    unshift: function (content) {
+        'use strict';
+        // Create new node, where next is _head, and previous is undefined
+        var node = new Node(content, this._head, undefined);
+
+        if (this._length === 0) {
+            this._head = this._tail = node;
+        } else {
+            this._head.prev = node;
+            this._head = node;
+        }
+
+        // No matter the content, this method will always add a Node
+        this._length += 1;
+
+        return this;
+    },
+
+    /**
+     * Remove the last Node of the DoublyLinkedList, and return that Node.
+     * @method pop
+     * @returns {Node|undefined}
+     */
+    pop: function () {
+        'use strict';
+        if (this._length === 0) {
+            // Empty list, short circuit
+            return undefined;
+        } else {
+            var node = this._tail;
+
+            this._tail = node.prev;
+            this._length -= 1;
+
+            if (this._length === 0) {
+                this._head = this._tail;
+            }
+
+            return node.content;
+        }
+    },
+
+    /**
+     * Remove the first Node of the DoublyLinkedList, and return that Node.
+     * @method shift
+     * @returns {Node|undefined}
+     */
+    shift: function () {
+        'use strict';
+        if (this._length === 0) {
+            // Empty list, short circuit
+            return undefined;
+        } else {
+            var node = this._head;
+
+            this._head = node.next;
+            this._length -= 1;
+
+            if (this._length === 0) {
+                this._tail = this._head;
+            }
+
+            return node.content;
+        }
+    },
+
+    /**
+     * Execute the provided callback for each node in this DoublyLinkedList
+     * @method forEach
+     * @param callback {function}
+     * @param [thisArg]
+     */
+    forEach: function (callback, thisArg) {
+        'use strict';
+        if (this._length > 0) {
+            var index = 0,
+                length = this._length,
+                breaker = false,
+                node = this._head;
+            while (index < length) {
+                if (callback.call(thisArg, node.content, index, this) === breaker) { break; }
+                node = node.next;
+                index += 1;
+            }
+        }
+    },
+
+    /**
+     * Execute the provided callback for each node in this DoublyLinkedList, starting from the tail
+     * @method forEach
+     * @param callback {function}
+     * @param [thisArg]
+     */
+    forEachRight: function (callback, thisArg) {
+        'use strict';
+        if (this._length > 0) {
+            var breaker = false,
+                length = this._length,
+                node = this._tail;
+            while (length) {
+                if (callback.call(thisArg, node.content, length, this) === breaker) { break; }
+                node = node.prev;
+                length -= 1;
+            }
+        }
+    }
+};
---
+++ doublyLinkedList/tests.js
@@ -0,0 +1,205 @@
+/*global DoublyLinkedList:false */
+$(document).ready(function () {
+    'use strict';
+    test('Node Constructor', function () {
+        var node = new Node('content', 'next', 'prev');
+
+        strictEqual(node.content, 'content');
+        strictEqual(node.next, 'next');
+        strictEqual(node.prev, 'prev');
+    });
+
+    test('DoublyLinkedList Constructor', function () {
+        var list = new DoublyLinkedList();
+
+        strictEqual(list._length, 0);
+        strictEqual(list._head, undefined);
+        strictEqual(list._tail, undefined);
+    });
+
+    test('DoublyLinkedList Push', function () {
+        var list = new DoublyLinkedList();
+
+        list.push(1);
+        strictEqual(list._length, 1);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 1);
+        strictEqual(list._head.next, undefined);
+
+        strictEqual(list._tail.prev, undefined);
+        strictEqual(list._tail.content, 1);
+        strictEqual(list._tail.next, undefined);
+
+        list.push(2);
+        strictEqual(list._length, 2);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 1);
+        strictEqual(list._head.next.content, 2);
+
+        strictEqual(list._tail.prev.content, 1);
+        strictEqual(list._tail.content, 2);
+        strictEqual(list._tail.next, undefined);
+
+        list.push(3);
+        strictEqual(list._length, 3);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 1);
+        strictEqual(list._head.next.content, 2);
+
+        strictEqual(list._tail.prev.content, 2);
+        strictEqual(list._tail.content, 3);
+        strictEqual(list._tail.next, undefined);
+    });
+
+    test('DoublyLinkedList chain Push', function () {
+        var list = new DoublyLinkedList();
+
+        list.push(1).push(2).push(3);
+        strictEqual(list._length, 3);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 1);
+        strictEqual(list._head.next.content, 2);
+
+        strictEqual(list._tail.prev.content, 2);
+        strictEqual(list._tail.content, 3);
+        strictEqual(list._tail.next, undefined);
+    });
+
+    test('DoublyLinkedList Pop', function () {
+        var list = new DoublyLinkedList(),
+            result;
+
+        list.push(1).push(2).push(3);
+        result = list.pop();
+        strictEqual(list._length, 2);
+        strictEqual(result, 3);
+
+        result = list.pop();
+        strictEqual(list._length, 1);
+        strictEqual(result, 2);
+
+        result = list.pop();
+        strictEqual(list._length, 0);
+        strictEqual(result, 1);
+        strictEqual(list._head, undefined);
+        strictEqual(list._tail, undefined);
+
+        result = list.pop();
+        strictEqual(list._length, 0);
+        strictEqual(result, undefined);
+        strictEqual(list._head, undefined);
+        strictEqual(list._tail, undefined);
+
+    });
+
+    test('DoublyLinkedList Shift', function () {
+        var list = new DoublyLinkedList(),
+            result;
+
+        list.push(4).push(5).push(6);
+
+        result = list.shift();
+        strictEqual(list._length, 2);
+        strictEqual(result, 4);
+
+        result = list.shift();
+        strictEqual(list._length, 1);
+        strictEqual(result, 5);
+
+        result = list.shift();
+        strictEqual(list._length, 0);
+        strictEqual(result, 6);
+        strictEqual(list._head, undefined);
+        strictEqual(list._tail, undefined);
+
+        result = list.shift();
+        strictEqual(list._length, 0);
+        strictEqual(result, undefined);
+        strictEqual(list._head, undefined);
+        strictEqual(list._tail, undefined);
+    });
+
+    test('DoublyLinkedList Unshift', function () {
+        var list = new DoublyLinkedList();
+
+        list.unshift(7);
+        strictEqual(list._length, 1);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 7);
+        strictEqual(list._head.next, undefined);
+
+        strictEqual(list._tail.prev, undefined);
+        strictEqual(list._tail.content, 7);
+        strictEqual(list._tail.next, undefined);
+
+        list.unshift(8);
+        strictEqual(list._length, 2);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 8);
+        strictEqual(list._head.next.content, 7);
+
+        strictEqual(list._tail.prev.content, 8);
+        strictEqual(list._tail.content, 7);
+        strictEqual(list._tail.next, undefined);
+
+        list.unshift(9);
+        strictEqual(list._length, 3);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 9);
+        strictEqual(list._head.next.content, 8);
+
+        strictEqual(list._tail.prev.content, 8);
+        strictEqual(list._tail.content, 7);
+        strictEqual(list._tail.next, undefined);
+    });
+
+    test('DoublyLinkedList chain Unshift', function () {
+        var list = new DoublyLinkedList();
+
+        list.unshift(7).unshift(8).unshift(9);
+        strictEqual(list._length, 3);
+
+        strictEqual(list._head.prev, undefined);
+        strictEqual(list._head.content, 9);
+        strictEqual(list._head.next.content, 8);
+
+        strictEqual(list._tail.prev.content, 8);
+        strictEqual(list._tail.content, 7);
+        strictEqual(list._tail.next, undefined);
+    });
+
+    test('DoublyLinkedList forEach', function () {
+        var list = new DoublyLinkedList(),
+            result = [];
+
+        list.unshift(7).unshift(8).unshift(9);
+        list.forEach(function (content) {
+            result.push(content);
+        });
+        strictEqual(result.length, 3);
+        strictEqual(result[0], 9);
+        strictEqual(result[1], 8);
+        strictEqual(result[2], 7);
+    });
+
+    test('DoublyLinkedList forEachRight', function () {
+        var list = new DoublyLinkedList(),
+            result = [];
+
+        list.unshift(7).unshift(8).unshift(9);
+        list.forEachRight(function (content) {
+            result.push(content);
+        });
+        strictEqual(result.length, 3);
+        strictEqual(result[0], 7);
+        strictEqual(result[1], 8);
+        strictEqual(result[2], 9);
+    });
+});
